8c2ca4c48b4e1006b2207c0cb4012a4cf0d7f40c
Ayman Soliman <aymansoliman.dev@gmail.com>
2026-04-01 16:19:43 +0200

    Update button class for ingredient input

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -9,10 +9,10 @@ export default function Inputs({ handleAddIngredient }) {
                         id="ingredient"
                         placeholder="e.g. oregano"
                         aria-label="Add ingredient"
-                        className="w-full border-2 border-gray-300 rounded-md p-2 placeholder-gray-500 box"
+                        className="w-full border-2 border-gray-300 rounded-md p-2 placeholder-gray-500 box sketchy-sm"
                     />
                 </label>
-                <button onClick={(e) => {e.preventDefault(); handleAddIngredient(document.querySelector('[name="ingredient"]'))}} className="shrink-0 cursor-pointer bg-white text-black border-2 border-dashed rounded-md py-2 px-4 active:scale-[0.98] transition ease-in duration-150">+ Add ingredient</button>
+                <button onClick={(e) => {e.preventDefault(); handleAddIngredient(document.querySelector('[name="ingredient"]'))}} className="shrink-0 cursor-pointer bg-white text-black border-2 border-dashed rounded-md py-2 px-4 active:scale-[0.98] transition ease-in duration-150 sketchy-pill">+ Add ingredient</button>
             </div>
         </>
     );
