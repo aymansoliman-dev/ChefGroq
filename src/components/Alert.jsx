e83aee0a7cf9152c4f216de5896787bd914fbbf4
Ayman Soliman <aymansoliman.dev@gmail.com>
2026-04-01 16:23:32 +0200

    Add 'sketchy' class to Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -17,7 +17,7 @@ export default function Alert({ alertMessage, handleDismiss }) {
             aria-describedby="alert-dialog-description"
             ref={dialogRef}
             onClose={handleDismiss}
-            className="m-auto max-w-[90dvw] text-xl bg-white border-2 border-gray-900 p-8 pt-12 pr-12 backdrop:bg-[#00000077]"
+            className="m-auto max-w-[90dvw] text-xl bg-white border-2 border-gray-900 p-8 pt-12 pr-12 backdrop:bg-[#00000077] sketchy"
         >
             <form method="dialog">
                 <button aria-label="dismiss alert" className="absolute right-4 top-4 cursor-pointer">
